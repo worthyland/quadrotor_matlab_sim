cd010100a862290702b35df24207d2fdeef29faa
sw <shuangwenfan@hnu.edu.cn>
2022-11-11 16:27:24 +0800

    ok

--- model/mdl_quad.m
+++ model/mdl_quad.m
@@ -22,3 +22,9 @@ quadrotor.Ixx = 0.064;
 quadrotor.Iyy = 0.064;
 quadrotor.Izz = 0.112;
 quadrotor.J = diag([quadrotor.Ixx quadrotor.Iyy quadrotor.Izz]);    %   I       Flyer rotational inertia matrix     3x3
+
+quadrotor.h = -0.007;                 %   h       Height of rotors above CoG          1x1
+quadrotor.d = 0.315;                  %   d       Length of flyer arms                1x1
+
+%Rotor
+quadrotor.r = 0.165;                  %   r       Rotor radius                        1x1
